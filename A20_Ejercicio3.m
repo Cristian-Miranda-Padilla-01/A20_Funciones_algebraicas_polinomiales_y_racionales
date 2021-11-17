d3a2743c0bf1e92f7eed95fad9e67592d2b30fbc
CristianMirandaPadilla3101 <91565020+CristianMirandaPadilla3101@users.noreply.github.com>
2021-11-17 14:11:45 -0600

    Add files via upload

---
+++ A20_Ejercicio3.m
@@ -0,0 +1,26 @@
+% Octave Script
+%School                :Tecnologico de Estudios Superiores de Jilotepec 
+%Specialty             :Ingeniería en Sistemas Computacionales 
+% Title                :Funciones algebraicas: polinomicas y racionales
+% Author               :Cristan Miranda Padilla
+% Date                 :202123107  
+%Grupo                 :3101
+%Limpiamos variables
+clear
+pkg load symbolic
+syms x
+x=(-5:0.1:5);
+disp ('valor de la funcion');
+disp ('(f(x)=(x+2)./(x-1)');
+disp ('Esta es una funcion racional');
+fx=((x+2)./(x-1));
+disp ('Graficacion de la funcion');
+plot (x, fx);
+
+grid on;
+%Titulo
+title('FUNCION PROBLEMA 3');
+%Etiqueta para x
+xlabel('Eje "X"');
+%Etiqueta para y
+ylabel('Eje "Y"');
---
+++ A20_Ejercicio5.m
@@ -0,0 +1,27 @@
+% Octave Script
+%School                :Tecnologico de Estudios Superiores de Jilotepec 
+%Specialty             :Ingeniería en Sistemas Computacionales 
+% Title                :Funciones algebraicas: polinomicas y racionales
+% Author               :Cristan Miranda Padilla
+% Date                 :202123107  
+%Grupo                 :3101
+%Limpiamos variables
+clear
+pkg load symbolic
+syms x
+x=(-10:1:10);
+disp ('valor de la funcion');
+disp ('f(x)=³?x^2');
+disp ('f(x)= x^2/3');
+disp ('Esta es una funcion radical dado su exponente fraccionario');
+fx=(cbrt(x.^2))
+disp ('Graficacion de la funcion');
+plot (x, fx);
+
+grid on;
+%Titulo
+title('FUNCION PROBLEMA 5');
+%Etiqueta para x
+xlabel('Eje "X"');
+%Etiqueta para y
+ylabel('Eje "Y"');
